973aa6acc8df8708ecd182f5b4edabc127c252d7
Ken Thumi <kenthumi@gmail.com>
2021-05-28 05:53:40 +0300

    Project model added

--- websites/models.py
+++ websites/models.py
@@ -31,3 +31,36 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+
+class Project(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=60)
+    link = models.CharField(max_length=200)
+    description = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='projects')
+    
+    
+
+
+    def save_image(self):
+        return self.save()
+       
+
+    @classmethod
+    def update_caption(cls,update_details,id):
+        return cls.objects.filter(id=int(id)).update(image=update_details['image'],
+                                               name=update_details['name'],
+                                               caption=update_details['caption'],
+                                               profile=update_details['profile'])
+    
+
+
+    def delete_image(self):
+        return self.delete()
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'Image: {self.title} '
