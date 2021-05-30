fd64ad45a25d37e0bcbc63bfdeea410e9e6f386b
Ken Thumi <kenthumi@gmail.com>
2021-05-30 11:41:56 +0300

    modified setting>Secret key to .env

--- awards/settings.py
+++ awards/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n97dh31atru@^d*dtdi^1df08w9v50u&k%)71a6d^_xt1bd@k0'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
